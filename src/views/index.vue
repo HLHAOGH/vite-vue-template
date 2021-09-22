47ae46191176105babe5db425461a01e68a39712
李浩（技术-西安） <lihaojsxa@dangdang.com>
2021-09-22 14:44:26 +0800

    setup opt

--- src/views/index.vue
+++ src/views/index.vue
@@ -15,26 +15,35 @@
   destroyed    onUnmounted    组件销毁后
 
 */
-import { ref, toRef, toRefs, reactive } from 'vue'
+import {
+  ref,
+  toRef,
+  toRefs,
+  reactive,
+  defineEmits,
+  defineExpose,
+  defineProps,
+  useAttrs,
+  useSlots
+} from 'vue'
 // import导入css，在setup中作为变量使用
 // toRef 将某个对象转换为ref对象；toRefs 将对象的所有属性都转为ref对象
-export default {
-  data() {
-    return {
-      state: {
-        name: 'wa'
-      }
-    }
-  }
-  // 在创建组件实例时调用,return返回变量，其他生命周期函数在setup()函数里面声明
-  // setup(props, ctx) {
-  //   const state = reactive({
-  //     name: 'li',
-  //     value: 4
-  //   })
-  //   return {
-  //     state
-  //   }
-  // }
-}
+/*
+  defineEmits // 定义触发父组件事件
+  defineExpose // 为父组件暴露子组件数据
+  defineProps // 定义子组件属性
+  useAttrs // 使用父组件未在defineProps中定义的属性
+  useSlots // 使用父组件插槽数据 useSlots().default (默认|插槽名称)
+*/
+
+// 在创建组件实例时调用,return返回变量，其他生命周期函数在setup()函数里面声明
+// setup(props, ctx) {
+//   const state = reactive({
+//     name: 'li',
+//     value: 4
+//   })
+//   return {
+//     state
+//   }
+// }
 </script>
